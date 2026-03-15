06e8dd6a2fd51555fc11dfda2f9da0ce44fef2bc
Pratham Shukla <prathamshukla.star@gmail.com>
2026-03-16 02:34:28 +0530

    client redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import fs from 'fs'
-import path from 'path'
-
-export default function Home({ html }: { html: string }) {
-  return <div dangerouslySetInnerHTML={{ __html: html }} />
-}
-
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), 'public', 'index.html')
-  const html = fs.readFileSync(filePath, 'utf8')
-  return { props: { html } }
+export default function Home() {
+  if (typeof window !== 'undefined') {
+    window.location.href = '/index.html'
+  }
+  return null
 }
