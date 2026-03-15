54756ddd5bac236fb19e4381f2f38ac915df064a
Pratham Shukla <prathamshukla.star@gmail.com>
2026-03-16 02:28:02 +0530

    serve html properly

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { redirect } from 'next/navigation'
+import fs from 'fs'
+import path from 'path'
 
-export default function Home() {
-  redirect('/index.html')
+export default function Home({ html }: { html: string }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'public', 'index.html')
+  const html = fs.readFileSync(filePath, 'utf8')
+  return { props: { html } }
 }
