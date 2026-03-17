e2c046abfcdae35e670bb17ed63a6ce57506e8ac
Pratham Shukla <prathamshukla.star@gmail.com>
2026-03-18 02:06:41 +0530

    switch to static

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,13 @@
-import { useEffect } from 'react'
-
 export default function Home() {
-  useEffect(() => {
-    document.title = 'Socialyft AI'
-  }, [])
-
   return (
-    <iframe
-      src="/index.html"
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        border: 'none'
-      }}
-    />
+    <>
+      <style>{`
+        * { margin:0; padding:0; box-sizing:border-box; }
+        body { background:#080808; }
+      `}</style>
+      <script dangerouslySetInnerHTML={{__html: `
+        window.location.replace('/index.html');
+      `}} />
+    </>
   )
 }
